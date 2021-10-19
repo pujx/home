53436113b8ff86a3ab17d1a22bb6f868593021cd
pujx <631746321@qq.com>
2021-10-19 19:07:45 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,3 +5,32 @@
 //			'overflow': 'visible'
 //		});
 	})
+	
+
+function IEVersion() {
+            var userAgent = navigator.userAgent;   
+            var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1;
+            var isEdge = userAgent.indexOf("Edge") > -1 && !isIE; 
+            var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
+            if(isIE) {
+            	
+               $('body').addClass('ie-browser');
+                var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+                reIE.test(userAgent);
+                var fIEVersion = parseFloat(RegExp["$1"]);
+                if(fIEVersion <= 10) {
+                    alert("10");
+               	$('body').addClass('ie-browser');
+                    return 7;
+                }  
+            } else if(isEdge) {
+                return 'edge';
+               $('body').addClass('ie-browser');
+            } else if(isIE11) {
+               $('body').addClass('ie-browser');
+                return 11;
+            }else{
+               $('body').removeClass('ie-browser');
+            }
+        }
+IEVersion();
